8fac0c1d5d085599650b706e1f7ee134f21a655f
nick <w@kucharski.ovh>
2025-06-12 14:00:24 +0200

    second commit, testing window resize

--- src/main/java/UI/MainFrame.java
+++ src/main/java/UI/MainFrame.java
@@ -5,6 +5,10 @@ import java.awt.*;
 
 public class MainFrame extends JFrame {
     public MainFrame() {
+
+        ImageIcon mailIcon = new ImageIcon(getClass().getResource("/addmail.jpg"));
+        ImageIcon contactIcon = new ImageIcon(getClass().getResource("/addcontact.jpg"));
+
         JPanel panel = new JPanel(new GridBagLayout());
         GridBagConstraints gbc = new GridBagConstraints();
         gbc.insets = new Insets(5, 5, 5, 5);
@@ -13,60 +17,128 @@ public class MainFrame extends JFrame {
         setTitle("SimpleMail");
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
-        JPanel topPanel = new JPanel();
+        JPanel topPanel = new JPanel(new GridBagLayout());
         topPanel.setBackground(Color.LIGHT_GRAY);
         topPanel.setBorder(BorderFactory.createLineBorder(Color.DARK_GRAY));
-        topPanel.add(new JButton("b1"));
-        topPanel.add(new JButton("b2"));
+
+        GridBagConstraints gbc_topPanel = new GridBagConstraints();
+        gbc_topPanel.insets = new Insets(10, 10, 10, 10);
+        gbc_topPanel.gridy = 0;
+
+        gbc_topPanel.gridx = 0;
+        gbc_topPanel.anchor = GridBagConstraints.WEST;
+
+        JButton mailButton = new JButton("Send Mail");
+        mailButton.setPreferredSize(new Dimension(200, 100));
+        mailButton.setIcon(mailIcon);
+        mailButton.setHorizontalTextPosition(SwingConstants.CENTER);
+        mailButton.setVerticalTextPosition(SwingConstants.BOTTOM);
+        mailButton.setBackground(Color.WHITE);
+        mailButton.setFont(new Font(Font.MONOSPACED, Font.BOLD, 20));
+        topPanel.add(mailButton, gbc_topPanel);
+
+        gbc_topPanel.gridx = 1;
+        JButton contactButton = new JButton("Add a Contact");
+        contactButton.setPreferredSize(new Dimension(200, 100));
+        contactButton.setIcon(contactIcon);
+        contactButton.setHorizontalTextPosition(SwingConstants.CENTER);
+        contactButton.setVerticalTextPosition(SwingConstants.BOTTOM);
+        contactButton.setBackground(Color.WHITE);
+        contactButton.setFont(new Font(Font.MONOSPACED, Font.BOLD, 20));
+        topPanel.add(contactButton, gbc_topPanel);
+
+        gbc_topPanel.gridx = 2;
+        gbc_topPanel.weightx = 1.0;
+        gbc_topPanel.fill = GridBagConstraints.HORIZONTAL;
+        topPanel.add(Box.createHorizontalGlue(), gbc_topPanel);
+
+        JLabel clockLabel = new JLabel("Clock");
+        clockLabel.setFont(new Font(Font.MONOSPACED, Font.BOLD, 40));
+        gbc_topPanel.gridx = 3;
+        gbc_topPanel.anchor = GridBagConstraints.EAST;
+        gbc_topPanel.weightx = 0;
+        gbc_topPanel.fill = GridBagConstraints.NONE;
+        topPanel.add(clockLabel, gbc_topPanel);
+        
         gbc.gridx = 0;
         gbc.gridy = 0;
         gbc.gridwidth = 2;
         gbc.gridheight = 1;
         gbc.weightx = 1.0;
-        gbc.weighty = 0.1;
+        gbc.weighty = 0.05;
         panel.add(topPanel, gbc);
 
         JPanel leftPanel = new JPanel();
         leftPanel.setBackground(Color.LIGHT_GRAY);
         leftPanel.setBorder(BorderFactory.createLineBorder(Color.DARK_GRAY));
         leftPanel.setLayout(new BoxLayout(leftPanel, BoxLayout.Y_AXIS));
-        leftPanel.add(new JButton("b3"));
-        leftPanel.add(new JButton("b4"));
+        JLabel wyslaneLabel = new JLabel("Sent Messages::");
+        wyslaneLabel.setFont(new Font(Font.MONOSPACED, Font.BOLD, 20));
+        leftPanel.add(wyslaneLabel, gbc);
+
+        leftPanel.add(new JButton("placeholder1"));
+        leftPanel.add(new JButton("placeholder2"));
         gbc.gridx = 0;
         gbc.gridy = 1;
         gbc.gridwidth = 1;
-        gbc.weightx = 0.2;
-        gbc.weighty = 0.9;
+        gbc.weightx = 0.1;
+        gbc.weighty = 0.95;
+
         panel.add(leftPanel, gbc);
 
         JPanel rightPanel = new JPanel();
+        rightPanel.setLayout(new GridBagLayout());
         rightPanel.setBackground(Color.WHITE);
         rightPanel.setBorder(BorderFactory.createLineBorder(Color.DARK_GRAY));
         GridBagConstraints innerGBC = new GridBagConstraints();
-        innerGBC.fill = GridBagConstraints.HORIZONTAL;
 
-        JTextField textField1 = new JTextField("Adresat:");
+        JLabel adresatLabel = new JLabel("ADRESAT:");
+        adresatLabel.setFont(new Font(Font.MONOSPACED, Font.BOLD, 20));
         innerGBC.gridx = 0;
         innerGBC.gridy = 0;
+        innerGBC.weightx = 0.15;
+        innerGBC.anchor = GridBagConstraints.WEST;
+        innerGBC.fill = GridBagConstraints.NONE;
+        rightPanel.add(adresatLabel, innerGBC);
+
+        JTextField textField1 = new JTextField();
+        textField1.setFont(new Font(Font.MONOSPACED, Font.PLAIN, 20)); // ustaw czcionkę
+        innerGBC.fill = GridBagConstraints.HORIZONTAL;
+        innerGBC.gridx = 1;
+        innerGBC.gridy = 0;
+        innerGBC.weightx = 0.85;
         rightPanel.add(textField1, innerGBC);
 
-        JTextField textField2 = new JTextField("Temat:");
+        JLabel topicLabel = new JLabel("TOPIC:");
+        topicLabel.setFont(new Font(Font.MONOSPACED, Font.BOLD, 20));
         innerGBC.gridx = 0;
         innerGBC.gridy = 1;
+        innerGBC.weightx = 0.15;
+        innerGBC.anchor = GridBagConstraints.WEST;
+        innerGBC.fill = GridBagConstraints.NONE;
+        rightPanel.add(topicLabel, innerGBC);
+
+        JTextField textField2 = new JTextField();
+        textField2.setFont(new Font(Font.MONOSPACED, Font.PLAIN, 20)); // ustaw czcionkę
+        innerGBC.fill = GridBagConstraints.HORIZONTAL;
+        innerGBC.gridx = 1;
+        innerGBC.gridy = 1;
+        innerGBC.weightx = 0.85;
         rightPanel.add(textField2, innerGBC);
 
-        JTextArea textArea = new JTextArea(10, 40);
+        JTextArea textArea = new JTextArea();
+        innerGBC.fill = GridBagConstraints.BOTH;
         JScrollPane scrollPane = new JScrollPane(textArea);
         innerGBC.gridx = 0;
         innerGBC.gridy = 2;
         innerGBC.gridwidth = 2;
-        innerGBC.fill = GridBagConstraints.BOTH;
+        innerGBC.weighty = 1;
         rightPanel.add(scrollPane, innerGBC);
 
         gbc.gridx = 1;
         gbc.gridy = 1;
-        gbc.weightx = 0.8;
-        gbc.weighty = 0.9;
+        gbc.weightx = 0.9;
+        gbc.weighty = 0.95;
         panel.add(rightPanel, gbc);
 
         pack();
@@ -74,5 +146,6 @@ public class MainFrame extends JFrame {
         setLocationRelativeTo(null);
         setResizable(true);
         setVisible(true);
+        setSize(1200, 800);
     }
 }
